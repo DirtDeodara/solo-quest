5ba4c6ae8639473f47545f330ae6de1c0cce12ce
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-08 12:40:35 -0700

    fixed some travis

--- src/services/quest-data.js
+++ src/services/quest-data.js
@@ -1,41 +1,43 @@
-const loadIn = {
-    id: 'loadIn',
-    title: 'Load In',
-    description: `The van is parked out front of the venue,
-    which is a bummer cus' you know your supposed to load in around back.
-    It would be some much closer to just load in the front door, though...
-    One of the band members say, "Alright, it's time to load in the gear. 
-    Lace up those boots, tighten that belt, and get to haulin'.
-    And don't forget the drum rug this time."`,
+//i dont have this tied in yet. just wanted to be able to PR
 
-    choices: [{
-        id: 'around-back',
-        description: `You decide to just do as your told and carry everything, 
-        load by heavy load, around back. `,
-        result: `It's a pain dragging those damn amps all the way around back
-        , but the drummer sees you working hard and after you're done they share 
-        the rest of their burrito with you.`,
-        hunger: -5,
-        fatigue: 20,
-    }, {
-        id: 'front-door',
-        description: `You really don't see why you should have to go all the way around.
-        There's a door right there and it would cut your work in half...Screw it, 
-        front door it is.`,
-        result: `You manage to not run into anyone as you squeeze yourself and the towering 
-        cabinet through the door and to the stage. "Easy peazy", you think. You quickly finish
-        loading in all the amps, but when you get back out to the van you see someone running 
-        off with one of the guitars. You chase after they and soon they ditch the loot and sprint off.`,
-        hunger: 5,
-        fatigue: 30,
-    }, {
-        id: 'helping-hands',
-        description: `As you're carrying a stack of drums through the back alley you see an old buddy.
-        They offer to help you in your task.`,
-        result: `You both make quick work of the load-in. It was great to have the help, and you end 
-        up catching up with your friend for a while. You lose track of time, though, and realize 
-        all-of-a-sudden that the show is starting any minute.`,
-        hunger: 10,
-        fatigue: 10,
-    }]
-};
+// const loadIn = {
+//     id: 'loadIn',
+//     title: 'Load In',
+//     description: `The van is parked out front of the venue,
+//     which is a bummer cus' you know your supposed to load in around back.
+//     It would be some much closer to just load in the front door, though...
+//     One of the band members say, "Alright, it's time to load in the gear. 
+//     Lace up those boots, tighten that belt, and get to haulin'.
+//     And don't forget the drum rug this time."`,
+
+//     choices: [{
+//         id: 'around-back',
+//         description: `You decide to just do as your told and carry everything, 
+//         load by heavy load, around back. `,
+//         result: `It's a pain dragging those damn amps all the way around back
+//         , but the drummer sees you working hard and after you're done they share 
+//         the rest of their burrito with you.`,
+//         hunger: -5,
+//         fatigue: 20,
+//     }, {
+//         id: 'front-door',
+//         description: `You really don't see why you should have to go all the way around.
+//         There's a door right there and it would cut your work in half...Screw it, 
+//         front door it is.`,
+//         result: `You manage to not run into anyone as you squeeze yourself and the towering 
+//         cabinet through the door and to the stage. "Easy peazy", you think. You quickly finish
+//         loading in all the amps, but when you get back out to the van you see someone running 
+//         off with one of the guitars. You chase after they and soon they ditch the loot and sprint off.`,
+//         hunger: 5,
+//         fatigue: 30,
+//     }, {
+//         id: 'helping-hands',
+//         description: `As you're carrying a stack of drums through the back alley you see an old buddy.
+//         They offer to help you in your task.`,
+//         result: `You both make quick work of the load-in. It was great to have the help, and you end 
+//         up catching up with your friend for a while. You lose track of time, though, and realize 
+//         all-of-a-sudden that the show is starting any minute.`,
+//         hunger: 10,
+//         fatigue: 10,
+//     }]
+// };
